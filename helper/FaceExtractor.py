aeba04757d3206fb3dc3ae2c3b7c4cba3bec0a0c
Tanase Victor-Flavian <flaviantanase30@gmail.com>
2024-03-11 21:42:12 +0200

    updated arg logic inside FaceExtractor

--- helper/FaceExtractor.py
+++ helper/FaceExtractor.py
@@ -43,9 +43,10 @@ class FaceExtractor:
 
         return x1, y1, size_bb
 
-    def extract_faces(self, bins = 10, sample_size = 10):
+    def extract_faces(self, num_bins = 10, sample_size = 10):
         """
-        Gets a video as input samples consecutive frames from a random location 
+        Gets a video as input and samples consecutive frames from a starting point
+        chosen randomly from a predefined list.
 
         """
         if os.path.exists(self.video_path):
@@ -66,21 +67,24 @@ class FaceExtractor:
             # Define reader object
             reader = cv2.VideoCapture(self.video_path + file)
 
+            # Check if reader works
             if not reader.isOpened():
                 print("Error: Could not open video.")
                 exit()
             
-            # Get the number of frames 
+            # Number of frames of the video
             num_frames = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
             
+            # Check if video file is empty
             if num_frames == 0:
                 print("Error: Video file possibly empty.")
                 exit()
 
+            # HOG based face detector
             face_detector = dlib.get_frontal_face_detector()
 
             # Create a list of starting points in the video to sample from
-            frame_bins = np.linspace(1, num_frames, bins, dtype=int)
+            frame_bins = np.linspace(1, num_frames, num_bins, dtype=int)
             
             # Randomly select the starting point for sampling frames
             start_frame = random.choice(frame_bins)
@@ -101,7 +105,7 @@ class FaceExtractor:
 
                 height, width = frame.shape[:2]
 
-                # Detect faces with dlib
+                # Detect faces 
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 detected_faces = face_detector(gray, 1)
 
@@ -110,7 +114,8 @@ class FaceExtractor:
                     
                     frames_captured += 1
                     self.total_frames_captured += 1
-                    # Process the biggest face
+
+                    # Process only the biggest face
                     face = detected_faces[0]
 
                     # Get the bounding box of the face
@@ -123,8 +128,6 @@ class FaceExtractor:
                     cv2.imwrite(
                         f"{self.output_path}/{name_creator(self.total_frames_captured)}.", cropped_face)
         
-      
-
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(
@@ -135,10 +138,20 @@ if __name__ == "__main__":
 
     p.add_argument('--output_path', '-o',
                    type=str,
-                   default='.',
+                   default='./Extracted_Faces/',
                    help='Path to the directory where data is saved')
+
+    p.add_argument("--num_bins", '-b',
+                   type=int,
+                   default=10,
+                   help="Number of starting points in the video to sample from")
+    
+    p.add_argument("--sample_size", '-s',
+                    type=int,
+                    default=10,
+                    help="Number of frames to sample from the videos")
 
     args = p.parse_args()
 
     faceExtractor = FaceExtractor(args.video_path, args.output_path)
-    faceExtractor.extract_faces(args.start_frame, args.end_frame)
+    faceExtractor.extract_faces(p.num_bins, p.sample_size)
