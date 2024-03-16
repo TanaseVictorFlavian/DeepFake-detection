e8c822706a727940f10759d658ced2d5157ffedf
Tanase Victor-Flavian <flaviantanase30@gmail.com>
2024-03-16 13:24:48 +0200

    fixed bug in face collection creator

--- utils/create_face_collection.py
+++ utils/create_face_collection.py
@@ -9,13 +9,23 @@ the FaceForensics++ dataset beforehand.
 class VideoDataset:
     def __init__(self, name, in_root, out_root, subdirs = None, num_bins = 20, sample_size = 3):
         self.name = name
-        self.root = in_root
+        self.in_root = in_root
         self.out_root = out_root
         self.num_bins = num_bins   
         self.sample_size = sample_size
         self.subdirs = subdirs
+        self.in_paths = self.generate_in_paths()
         self.out_paths = self.generate_out_paths()
     
+    def generate_in_paths(self):
+        if self.subdirs is None:
+            return self.in_root
+
+        in_paths = {}
+        for subdir in self.subdirs:
+            in_paths[subdir] = self.in_root + self.name + "/" + subdir + "/"
+        return in_paths
+   
     def generate_out_paths(self):
         if self.subdirs is None:
             return self.out_root + self.name + "/"
@@ -24,6 +34,7 @@ class VideoDataset:
         for subdir in self.subdirs:
             out_paths[subdir] = self.out_root + self.name + "/" + subdir + "/"
         return out_paths
+    
 # def run_downloader():
     # # Path to the downloader
     # # More info about the downloader: https://github.com/ondyari/FaceForensics/blob/master/dataset/README.md
@@ -39,36 +50,7 @@ if __name__ == "__main__":
     # child.start()
     # child.join()
 
-    # ROOT_INPUT_DIR = "../VideoData"
-    # ROOT_OUTPUT_DIR = "../ExtractedFaces"
-    # # FORENSICS PATHS
-    # TECHNIQUES = ["NeuralTextures", "FaceShifter",
-    #               "Deepfakes", "Face2Face", "FaceSwap"]
-    # COMPRESSION = ["c23"]
-    # FORENSICS_INPUT_PATHS = [
-    #     f"{ROOT_INPUT_DIR}/Forensics_pp/manipulated_sequences/{x}/{y}/videos/" for x in TECHNIQUES for y in COMPRESSION] + [f"{ROOT_INPUT_DIR}/Forensics_youtube/"]
-
-    # FORENSICS_OUTPUT_PATHS = [
-    #     f"{ROOT_OUTPUT_DIR}/Forensics_pp/{x}/" for x in TECHNIQUES] + [f"{ROOT_OUTPUT_DIR}/Forensics_youtube/"]
-    
-    # # CELEB-DF2 PATHS
-    # CELEB = ["CelebDF", "Celeb-real", "YouTube-real"]
-
-    # # INPUT PATHS
-    # INPUT_PATHS = [f"{ROOT_INPUT_DIR}/{x}/" for x in CELEB] + FORENSICS_INPUT_PATHS
-    # OUTPUT_PATHS = [f"{ROOT_OUTPUT_DIR}/{x}/" for x in CELEB] + FORENSICS_OUTPUT_PATHS
-    # PARAMS = []
-    # for in_path, out_path, param in zip(INPUT_PATHS, OUTPUT_PATHS, PARAMS):
-    #     os.system(
-    #         f"python ./utils/FaceExtractor_random_frames.py -i {in_path} -o {out_path}")
-        
-    # Forensics = {
-    #     "subdirs": ["NeuralTextures", "FaceShifter", "Deepfakes", "Face2Face", "FaceSwap"],
-    #     "num_bins": 10,
-    #     "sample_size": 10
-    # }
-        
-# os.system(f"python ./utils/FaceExtractor_random_frames.py -i {in_path} -o {out_path} -b {Forensics["num_bins"]} -s {Forensics["sample_size"]}")
+    # os.system(f"python ./utils/FaceExtractor_random_frames.py -i {in_path} -o {out_path} -b {Forensics["num_bins"]} -s {Forensics["sample_size"]}")
 
     #Negative samples
     Forensics = VideoDataset("Forensicspp", "../VideoData/Forensics_pp/", "./ExtractedFaces/", 
@@ -87,7 +69,8 @@ if __name__ == "__main__":
             for subdir in ds.out_paths:
                 # os.system(f"python ./utils/FaceExtractor_random_frames.py -i {ds.root} -o {ds.out_paths[subdir]} -b {ds.num_bins} -s {ds.sample_size}")
                 print(
-                    f"python ./utils/FaceExtractor_random_frames.py -i {ds.root} -o {ds.out_paths[subdir]} -b {ds.num_bins} -s {ds.sample_size}")
+                    f"python ./utils/FaceExtractor_random_frames.py -i {ds.in_paths[subdir]} -o {ds.out_paths[subdir]} -b {ds.num_bins} -s {ds.sample_size}")
         else:  
             print(
-                f"python ./utils/FaceExtractor_random_frames.py -i {ds.root} -o {ds.out_paths} -b {ds.num_bins} -s {ds.sample_size}")
+                f"python ./utils/FaceExtractor_random_frames.py -i {ds.in_root} -o {ds.out_paths} -b {ds.num_bins} -s {ds.sample_size}")
+    # os.system("python ./utils/FaceExtractor_random_frames.py -i ../VideoData/Forensics_pp/ -o ./ExtractedFaces/Forensicspp/NeuralTextures/ -b 20 -s 2")
