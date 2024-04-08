9c81968b9360a519128c100262ff684d2a9a44bc
Tanase Victor-Flavian <flaviantanase30@gmail.com>
2024-04-08 22:00:41 +0300

    fixed another conflict

--- app/app.py
+++ app/app.py
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask, request, render_template, jsonify
-from werkzeug.utils import secure_filename
-import os
-
-app = Flask(__name__)
-
-# Ensure there's a folder to save the uploaded files
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    if file:
-        filename = secure_filename(file.filename)
-
-    
-        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(save_path)
-        # Now you can use the file path to process the file using your ML model
-        return jsonify({'message': 'File successfully uploaded'}), 200
-
-
-if __name__ == '__main__':
-    # Create the upload folder if it doesn't exist
-
-    # video_formats = ['mp4', 'avi', 'mov', 'flv', 'wmv', 'mkv']
-    image_formats = ['png', 'jpg', 'jpeg']
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(debug=True)
-=======
 from flask import Flask, request, render_template
 from helpers.get_model import get_model
 from werkzeug.utils import secure_filename
@@ -97,4 +51,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
->>>>>>> 96cd39c52ea6416058e985ff72c591ceb41cf243
