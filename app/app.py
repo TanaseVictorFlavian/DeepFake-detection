4c30e8a3daddc6a10422ba4fed8303b7a46398ad
Tanase Flavian <flaviantanase30@gmail.com>
2024-05-26 12:02:31 +0300

    moved upload folder, added button for prediciton in app

--- app/app.py
+++ app/app.py
@@ -3,15 +3,16 @@ import sys
 
 sys.path.insert(0, 'D:\AA_Licenta\DeepFake-detection')
 
-from flask import Flask, request, render_template
-from helpers.get_model import get_model
-from werkzeug.utils import secure_filename
-from helpers.prepare_data import prepare_image, prepare_video
-from helpers.prediction import get_prediction
+import shutil
 import torch
-import shutil 
+from helpers.prediction import get_prediction
+from helpers.prepare_data import prepare_image, prepare_video
+from werkzeug.utils import secure_filename
+from helpers.get_model import get_model
+from flask import Flask, request, render_template
 
-UPLOAD_FOLDER = './uploads'
+
+UPLOAD_FOLDER = './app/static/uploads'
 VIDEO_FORMATS = {'mp4', 'avi', 'mov', 'flv', 'wmv'}
 IMAGE_FORMATS = {'jpg', 'jpeg', 'png', 'bmp'}
 ALLOWED_FORMATS = VIDEO_FORMATS.union(IMAGE_FORMATS)
@@ -20,54 +21,43 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-
 def is_allowed(filename) -> bool:
-    file_format =  filename.rsplit('.', 1)[1].lower() 
+    file_format = filename.rsplit('.', 1)[1].lower()
     allowed = '.' in filename and file_format in ALLOWED_FORMATS
     return allowed, file_format
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         toggle_button = request.form.get('aug', False)
         tta_enabled = True if toggle_button == 'true' else False
-        
-        if 'file' not in request.files:
-            return render_template('index.html')
-        file = request.files['file']
 
-        if file.filename == '':
-            return render_template('index.html')
+        file_path = os.path.join("./app", request.form.get('uploaded_image'))
+        file_format = file_path.rsplit('.', 1)[1].lower()
 
-        allowed, file_format = is_allowed(file.filename)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        if file and allowed:
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            
-            device = "cuda" if torch.cuda.is_available() else "cpu"
+        model, transforms = get_model(
+            model_name="effnet_b0_pretrained",
+        )
 
-            model, transforms = get_model(
-                model_name="effnet_b0_pretrained",
-            )
+        prepared_data = prepare_video(UPLOAD_FOLDER, transforms, tta_enabled) if file_format in VIDEO_FORMATS \
+            else prepare_image(file_path, transforms)
 
-            prepared_data = prepare_video(UPLOAD_FOLDER, transforms, tta_enabled) if file_format in VIDEO_FORMATS \
-                            else prepare_image(file_path, transforms)
-            
-            print(f"Number of prepared files : {len(prepared_data)}")
-            deepfake, confidence = get_prediction(model, device, prepared_data)
+        print(f"Number of prepared files : {len(prepared_data)}")
+        deepfake, confidence = get_prediction(model, device, prepared_data)
 
-            try:
-                if file_format in VIDEO_FORMATS:
-                    shutil.rmtree(f"{UPLOAD_FOLDER}/frames")
-                os.remove(file_path)
-                print("File Removed!")
-            except Exception as e:
-                return(f"Error deleting file: {e}")
-            return render_template('index.html', deepfake = deepfake, confidence = confidence, show_data = True)
+        try:
+            if file_format in VIDEO_FORMATS:
+                shutil.rmtree(f"{UPLOAD_FOLDER}/frames")
+            os.remove(file_path)
+            print("File Removed!")
+        except Exception as e:
+            return (f"Error deleting file: {e}")
+        return render_template('index.html', deepfake=deepfake, confidence=confidence, show_data=True)
 
-    return render_template('index.html', show_data = False)
+    return render_template('index.html', show_data=False)
 
 
 @app.route('/upload_image', methods=['GET', 'POST'])
@@ -88,11 +78,11 @@ def upload_image():
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
 
-        return render_template('index.html', uploaded_image = filename)
+        return render_template('index.html', uploaded_image=filename)
 
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    
+
     app.run(host='0.0.0.0', debug=True)
--- app/static/upload-handler.js
+++ app/static/upload-handler.js
@@ -1,21 +1,9 @@
 document
     .getElementById("fileUpload")
     .addEventListener("change", function (event) {
-        var checkbox = document.getElementById("toggle");
-        if (checkbox.checked) {
-            console.log("Checkbox is checked.");
-            console.log("The value of the checkbox is: " + checkbox.value);
-        } else {
-            console.log("Checkbox is not checked.");
-        }
-
-        document.getElementById("loader-container").style.display = "block";
-        console.log(this);
-        console.log(this.files);
         event.preventDefault();
         var formData = new FormData();
         formData.append("file", this.files[0]);
-        formData.append("aug", checkbox.checked);
 
         fetch("http://127.0.0.1:5000/upload_image", {
             method: "POST",
@@ -36,3 +24,41 @@ document
                 alert("Error uploading file.");
             });
     });
+
+
+document
+.getElementById("runButton")
+.addEventListener("click", function (event) {
+    var checkbox = document.getElementById("toggle");
+    if (checkbox && checkbox.checked) {
+        console.log("Checkbox is checked.");
+        console.log("The value of the checkbox is: " + checkbox.value);
+    } else {
+        console.log("Checkbox is not checked.");
+    }
+
+    document.getElementById("loader-container").style.display = "block";
+    event.preventDefault();
+    var formData = new FormData();
+    formData.append("aug", checkbox.checked);
+
+    const image = document.getElementById("uploadedImage");
+    formData.append("file_path", image.src);
+    fetch("http://127.0.0.1:5000/", {
+        method: "POST",
+        body: formData,
+
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.text(); // Assuming the server responds with text/html
+        })
+        .then((html) => {
+            document.documentElement.innerHTML = html;
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+});
